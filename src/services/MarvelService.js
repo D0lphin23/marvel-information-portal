67f61d1e9c378e29ea66cfe86cd34aac7722b307
gorelik.e <gorelikev23@gmail.com>
2026-04-02 22:54:31 +0500

    - Реализация компонента рандомного персонажа

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
     _apiBase = "https://marvel-server-zeta.vercel.app/";
     _apiKey = "apikey=d4eecb0c66dedbfae4eab45d312fc1df";
+    _limit = 20;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -13,15 +14,35 @@ class MarvelService {
     };
 
     getAllCharacters = async () => {
-        return this.getResource(
-            `${this._apiBase}characters?limit=9&${this._apiKey}`,
+        const res = await this.getResource(
+            `${this._apiBase}characters?limit=${this._limit}&${this._apiKey}`,
         );
+
+        return res.data.results.map(this._transformCharacter);
     };
 
     getCharacter = async (id) => {
-        return this.getResource(
+        const res = await this.getResource(
             `${this._apiBase}characters/${id}?${this._apiKey}`,
         );
+
+        return this._transformCharacter(res.data.results[0]);
+    };
+
+    _transformCharacter = (char) => {
+        const description =
+            char?.description?.length > 235
+                ? `${char?.description?.slice(0, 235)}...`
+                : char?.description ||
+                  "There is no description for this character";
+
+        return {
+            name: char.name,
+            description: description,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        };
     };
 }
 
